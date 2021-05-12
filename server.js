5c7e3302f7ea8dbca08896eee9866fefab818c28
Nicolás Nieto <niconieto1506@gmail.com>
2021-05-12 01:03:18 -0500

    cambio 1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!!'))
+app.get('/', (req, res) => res.send('<h1>Hola mundo</h1>'))
 
 
 app.listen(3000);
