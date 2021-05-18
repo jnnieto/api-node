f4e3f92a0d5f15c2598ce973670a591b3fe5243f
Nicolás Nieto <niconieto1506@gmail.com>
2021-05-17 19:04:19 -0500

    Rutas de la API

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -5,5 +5,8 @@ const router = Router()
 
 router.get('/', proyectoController.obtenerProyectos)
 router.post('/', proyectoController.crearProyecto)
+router.get('/:id', proyectoController.obtenerProyecto)
+router.put('/:id', proyectoController.actualizarProyecto)
+router.delete('/:id', proyectoController.borrarProyecto)
 
 export default router
