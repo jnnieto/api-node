3f18c9926e67014a0b4007eac623bd47989fec04
Nicolás <niconieto1506@gmail.com>
2021-08-08 12:47:04 -0500

    Busquedas por otras colecciones

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -2,6 +2,8 @@ const { response, request } = require('express');
 const { ObjectId } = require('mongoose').Types;
 
 const Usuario = require('../models/Usuario');
+const Categoria = require('../models/Categoria');
+const Producto = require('../models/Producto');
 
 const coleccionesPermitidas = [
     'usuarios',
@@ -35,6 +37,50 @@ const buscarUsuarios = async (termino = '', res = response) => {
     
 }
 
+const buscarCategorias = async (termino = '', res = response) => {
+
+    const esMongoID = ObjectId.isValid(termino);
+
+    if (esMongoID) {
+        const categoria = await  Categoria.findById(termino);
+        res.json({
+            results: (categoria) ? [categoria] : []
+        })
+    }
+
+    // No tiene en cuenta expresiones regulares
+    const regex = new RegExp(termino, 'i')
+
+    // Buscar por el nombre de la categoria
+    const categorias = await Categoria.find({ name: regex, state: true});
+
+    res.json({
+        results: categorias
+    })
+}
+
+const buscarProductos = async (termino = '', res = response) => {
+
+    const esMongoID = ObjectId.isValid(termino);
+
+    if (esMongoID) {
+        const producto = await Producto.findById(termino).populate('category', 'name');
+        res.json({
+            results: (producto) ? [producto] : []
+        })
+    }
+
+    // No tiene en cuenta expresiones regulares
+    const regex = new RegExp(termino, 'i')
+
+    const productos = await Producto.find({ name: regex, state: true}).populate('category', 'name');
+
+    res.json({
+        results: productos
+    })
+
+}
+
 const buscar = async (req = request, res = response) => {
 
     const { coleccion, termino } = req.params;
@@ -51,11 +97,11 @@ const buscar = async (req = request, res = response) => {
             break;
 
         case 'categorias':
-            
+            buscarCategorias(termino, res);
             break;
         
         case 'productos':
-            
+            buscarProductos(termino, res);
             break;
     
         default:
